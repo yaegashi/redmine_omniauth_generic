e7ade828d3c418182cf7411cd1c9b4923cb926b0
YAEGASHI Takeshi <yaegashi@debian.org>
2019-01-03 01:56:57 +0900

    Add authorization logic and authorize method for azure_oauth2

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -16,7 +16,16 @@ class OmniauthController < AccountController
       redirect_to signin_path
       return
     end
+    provider = params[:provider].to_sym
+    uid = request.env['omniauth.auth'].uid
     info = request.env['omniauth.auth'].info
+    logger.info "OmniAuth authenticated: #{provider} #{uid} #{info.to_json}"
+    authorize_method = "omniauth_authorize_#{provider}"
+    if respond_to?(authorize_method) && !send(authorize_method, provider)
+      flash['error'] = l('omniauth.error.authorization_failure')
+      redirect_to signin_path
+      return
+    end
     user = User.joins(:email_addresses)
                .where('email_addresses.address' => info['email'], 'email_addresses.is_default' => true)
                .first_or_initialize
@@ -42,6 +51,20 @@ class OmniauthController < AccountController
       return
     end
     account_pending
+  end
+
+  def omniauth_authorize_azure_oauth2(provider)
+    args = OmniAuth::Builder.providers[provider] || [{}]
+    options = args[0]
+    ag = options[:authorized_member_groups]
+    return true unless ag
+    ag = [ag] unless ag.is_a?(Array)
+    mg = request.env['omniauth.auth'].extra.member_groups || []
+    logger.info "OmniAuth member groups: #{mg.to_json}"
+    ag.each do |g|
+      return true if mg.include?(g)
+    end
+    return false
   end
 
   def settings
